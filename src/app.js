44f8a063130c22a10d4dda5493fe9a7f20120a2d
Jose Rivera <JdotRivera87@gmail.com>
2025-02-10 18:38:17 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import GameScreen from "./src/screens/GameScreen";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Game" component={GameScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
 
