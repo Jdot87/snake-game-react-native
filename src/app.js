8b8946914ee29bc1f96796d0b9d5cf0d83d715af
Jose Rivera <JdotRivera87@gmail.com>
2025-02-11 09:51:55 -0500

    adding comments Update app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import MainMenu from "./src/screens/MainMenu"; // Import Main Menu
-import GameScreen from "./src/screens/GameScreen"; // Import Game Screen
+import { NavigationContainer } from "@react-navigation/native"; // Importing NavigationContainer to handle navigation
+import { createStackNavigator } from "@react-navigation/stack"; // Importing Stack Navigator to manage screen transitions
+import MainMenu from "./src/screens/MainMenu"; // Importing the Main Menu screen
+import GameScreen from "./src/screens/GameScreen"; // Importing the Game Screen
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator(); // Creating the stack navigator
 
 export default function App() {
     return (
-        <NavigationContainer>
+        <NavigationContainer> 
+            {/* Wrapping the navigation structure inside NavigationContainer */}
             <Stack.Navigator screenOptions={{ headerShown: false }}>
+                {/* Defining a Stack Navigator and hiding the header for cleaner UI */}
                 <Stack.Screen name="MainMenu" component={MainMenu} />
+                {/* Defining the Main Menu screen */}
                 <Stack.Screen name="Game" component={GameScreen} />
+                {/* Defining the Game Screen */}
             </Stack.Navigator>
         </NavigationContainer>
     );
